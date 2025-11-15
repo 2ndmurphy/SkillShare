b6ee3ac88cd44716b051f70a6a7d539c3d5d1d2a
2ndmurphy <muhammad.jibrilmuqoddas@gmail.com>
2025-11-15 21:29:00 +0700

    initiate database migration,model,controller

---
+++ app/Http/Controllers/RoomObjectiveController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RoomObjective;
+use Illuminate\Http\Request;
+
+class RoomObjectiveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RoomObjective $roomObjective)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RoomObjective $roomObjective)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RoomObjective $roomObjective)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RoomObjective $roomObjective)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_11_15_140504_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mentor_id')->constrained('users');
+            $table->foreignId('type_id')->constrained('room_types');
+            $table->string('title', 120);
+            $table->tinyText('description');
+            $table->enum('mode', ['online', 'offline', 'hybrid']);
+            $table->string('location', 100);
+            $table->dateTime('started_at');
+            $table->dateTime('end_at')->nullable();
+            $table->text('requirements')->nullable();
+            $table->enum('status', ['waiting', 'started', 'completed'])->default('waiting');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
