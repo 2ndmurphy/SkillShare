322affb3e457e07b8c69fb0bcac7411a4f36d4d5
2ndmurphy <muhammad.jibrilmuqoddas@gmail.com>
2025-11-21 20:01:29 +0700

    add: room material migration

---
+++ database/migrations/2025_11_21_125516_create_room_materials_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('PRAGMA foreign_keys = ON');
+
+        Schema::create('room_materials', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('room_id');
+            $table->string('title');
+            $table->text('description')->nullable();
+
+            // Tipe materi bisa salah satu dari: file, link, atau content
+            $table->string('file_path');
+            $table->string('link_url');
+            $table->text('content');
+
+            // Room punya banyak materi
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room_materials');
+    }
+};
