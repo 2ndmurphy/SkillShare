e9896d0fd64340b92e40972f6bb2d1258220f6a0
Rhea <ilhamkumbang07@gmail.com>
2025-11-30 22:06:31 +0700

    style(css): adjust button color

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -82,7 +82,7 @@
         <div class="flex items-center gap-4 pt-2">
             <button
                 type="submit"
-                class="inline-flex items-center justify-center rounded-full bg-teal-600 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-1"
+                class="inline-flex items-center justify-center rounded-full bg-teal-500 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-1"
             >
                 {{ __('Save') }}
             </button>
